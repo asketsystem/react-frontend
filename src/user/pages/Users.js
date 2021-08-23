39245e4c1a1f8109015a3d8af182b5068fc602f2
asketsystem <32412954+asketsystem@users.noreply.github.com>
2021-08-23 12:02:42 +0000

    Adding a UserItem Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users Works!</h2>;
+    const USERS = [
+        //{
+        //    id: 'u1', 
+        //    name: 'Franck Asket', 
+        //    image: 'https://www.pexels.com/photo/woman-in-green-blazer-and-black-hijab-7897258/', 
+        //    places: 3 
+        //}
+];
+
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
