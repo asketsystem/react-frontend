8fc5e029aebb1a3148c0cb41ad19a4596f12768b
asketsystem <32412954+asketsystem@users.noreply.github.com>
2021-08-23 16:39:23 +0000

    Adding Component

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
-import UsersItem from './UsersItem';
+import UserItem from './UserItem';
 import './UsersList.css';
 
 const UsersList = props => {
-    if (props.items.length === 0) {
-        return  (
-            <div className="center">
-                <h2>No users found.</h2>
-            </div>
-        );
-    }
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found.</h2>
+      </div>
+    );
+  }
 
-    return <ul>
-        {props.items.map(user => (
-            <UsersItem 
-                key={user.id} 
-                id={user.id} 
-                image={user.image} 
-                name={user.name} 
-                placeCount={user.places}  
-            />
-        ))}
+  return (
+    <ul className="users-list">
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
     </ul>
+  );
 };
 
 export default UsersList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,16 +3,17 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [
-        //{
-        //    id: 'u1', 
-        //    name: 'Franck Asket', 
-        //    image: 'https://www.pexels.com/photo/woman-in-green-blazer-and-black-hijab-7897258/', 
-        //    places: 3 
-        //}
-];
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Franck Asket',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
+  ];
 
-    return <UsersList items={USERS} />;
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
