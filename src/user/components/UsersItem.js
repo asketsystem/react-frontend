da1c913ff0e41c335a2cee43e248ae05beccb718
asketsystem <32412954+asketsystem@users.noreply.github.com>
2021-08-25 15:56:26 +0000

    Styling our App and more Components

--- src/user/components/UsersItem.js
+++ src/user/components/UsersItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Avatar from '../../shared/components/UIElements/Avatar';
 import './UsersItem.css';
 
 const UsersItem = props => {
@@ -7,7 +8,7 @@ const UsersItem = props => {
         <li className="user-item">
             <div className="user-item__content">
                 <div className="user-item__image">
-                    <img src={props.image} alt={props.name} />
+                    <Avatar image={props.image} alt={props.name} />
                 </div>
                 <div className="user-item__info">
                     <h2>{props.name}</h2>
